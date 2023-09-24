8b22056f6146b5b598ca07851f5409b74af5cffd
quochuy.dev@gmail.com <quochuy.dev@gmail.com>
2023-09-24 11:14:45 +0700

    update

--- server/main.ts
+++ server/main.ts
@@ -11,19 +11,11 @@ fastify.register(require('@fastify/cookie'), {
 });
 
 fastify.get('/api/v2', (request, reply) => {
-  reply.header('set-cookie', ['key2=value1; Max-Age=0;']);
-
-  reply.send({
-    hello: 'world',
-    cookies: request['cookies'],
-  });
+  reply.send({ hello: 'world', cookies: request['cookies'] });
 });
 
 fastify.post('/api/v2', (request, reply) => {
-  reply.send({
-    hello: 'world',
-    cookies: request['cookies'],
-  });
+  reply.send({ hello: 'world', cookies: request['cookies'] });
 });
 
 fastify.listen({ port: 3333 }, (err) => {
