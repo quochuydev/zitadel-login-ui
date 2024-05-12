f494c71e565b9e02ec8456a6b364f9e0a986bef0
quochuydev <quochuy.dev@gmail.com>
2024-05-12 19:04:13 +0700

    fix: remove next-auth

--- app/api/session/route.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth';
-import { NextResponse } from 'next/server';
-import { authOptions } from '#/services/backend/next-auth';
-
-export async function GET() {
-  const session = await getServerSession(authOptions);
-  return NextResponse.json(session);
-}
--- app/auth/[...nextauth]/route.ts
+++
@@ -1,3 +0,0 @@
-import { handler } from '#/services/backend/next-auth';
-
-export { handler as GET, handler as POST };
