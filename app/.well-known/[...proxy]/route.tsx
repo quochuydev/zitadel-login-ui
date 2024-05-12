e647bbb8008d1fa2e03af0f1fb680d099a6e04f1
quochuydev <quochuy.dev@gmail.com>
2024-05-12 16:32:44 +0700

    fix: update proxy

--- app/.well-known/[...proxy]/route.tsx
+++ app/.well-known/[...proxy]/route.tsx
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 
 export async function handler(request: NextRequest) {
   if (
-    ['oauth', 'oidc', 'idps/callback'].some((e) =>
+    ['.well-known', 'oauth', 'oidc', 'idps/callback'].some((e) =>
       request.nextUrl.href.includes(e),
     )
   ) {
